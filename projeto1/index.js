42fdcfb1cd9c0779ed46fd9b0073d3ca029a7de2
werean <wereanrx@gmail.com>
2021-07-13 18:09:31 -0300

    ejspart3

--- projeto1/index.js
+++ projeto1/index.js
@@ -18,9 +18,20 @@ if (artigo) {
     res.send("welcome to this shitt!!!!");
 }
 })
+
 app.get("/carrinho/",function(req,res){
     res.send("Todas as suas teste")
 });
+
+app.get("canal/youtube", function(req,res){
+    var canal - req.query["canal"];
+
+    if (canal) {
+        res.send(canal);
+    } else {
+        res.send("nenhum canal fornecido");
+    }l
+})
 
 app.get("/ola/:name/:altura/",function(req,res){
     var name = req.params.name
@@ -35,4 +46,19 @@ app.listen(4000,function(erro){
         console.log("Servidor iniciado com sucesso!");
     }
 })
+ 
+
+// AULA 23 PRA FRENTE 
+
+// const express = require("express");
+// const app = express();
+
+// // Estou dizendo para o express usar o EJS como view engine
+// app.set('view engine','ejs');
+
+// app.get("/",(req, res) => {
+//     res.render("index.ejs");
+// });
+
+// app.listen(8080,()=>{console.log("app rodando!");});
 
