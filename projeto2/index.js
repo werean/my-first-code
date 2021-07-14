7ea727d29c4d0d0d23bcfd6266c2f0ab3be60f51
werean <wereanrx@gmail.com>
2021-07-14 18:20:43 -0300

    ejsfished

--- projeto2/index.js
+++ projeto2/index.js
@@ -2,18 +2,26 @@ const express = require("express");
 const app = express();
 
 app.set('view engine','ejs');
+app.use(express.static('./'))
 
 app.get("/:nome/:lang",(req, res) => {
-    var nome = req.params.nome;
+    var nome = req.params.nome; 
     var lang = req.params.lang;
-    var exibirmsg = true;
+    var exibirmsg = false;
+    var produtos = [
+        {nome: "mouse", preco: 59.90},
+        {nome: "teclado", preco: 79.99},
+        {nome: "headset", preco: 110.00},
+        {nome: "monitor", preco: 179.69}
+    ]
     
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "mecpock",
         inscritos: 5000,
-        msg: exibirmsg
+        msg: exibirmsg,
+        produtos: produtos
         });
 });
 
