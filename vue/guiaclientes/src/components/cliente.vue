52479e7687cf83be94e8b52ccfa61d9a71e9c775
werean <wereanrx@gmail.com>
2021-07-21 18:18:51 -0300

    newstart

---
+++ vue/guiaclientes/src/components/cliente.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="Cliente">
+    <h2>Cliente aqui - componente aqui xd</h2>
+    <h1>QUALQUER COISA</h1>
+    <h2>dhuawhduawhdu</h2>
+    <h2>11111</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#Cliente {
+  color: crimson;
+  background-color: cyan;
+}
+
+#Cliente-nome {
+  color: darkorange;
+}
+</style>
---
+++ vue/guiaclientes/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  front-family: Avanir, Helvetica, Arial, sans-serif;
+
+}
+</style>
