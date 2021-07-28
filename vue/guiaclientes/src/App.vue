4a967aac2dd56f5a6e37355a652fd6fbf9fb2707
werean <wereanrx@gmail.com>
2021-07-28 18:30:05 -0300

    V-for aula 265

--- vue/guiaclientes/src/App.vue
+++ vue/guiaclientes/src/App.vue
@@ -1,30 +1,68 @@
 <template>
   <div id="app">
-    <input type="text" v-model="clientevictor.nome" />
-    <input type="text" v-model="clientevictor.email" />
-    <Cliente :cliente="clientevictor" :showage="true" />
-    <Cliente :cliente="clientevictor" :showage="true" />
-    <Cliente :cliente="clientevictor" :showage="false" />
-    <Cliente :cliente="clientevictor" :showage="false" />
+    <!-- abaixo esse "v-for" lê uma lista de dados e exibe de maneira dinamica -->
+    <div v-for="(client, index) in clients" :key="client.id">
+      <h4>{{ index + 1 }}</h4>
+      <Client :client="client" />
+      <hr />
+      <h4>Edição:</h4>
+      <input type="text" v-model="client.name" />
+      <input type="text" v-model="client.email" />
+    </div>
   </div>
 </template>
 
 <script>
-import Cliente from "./components/Cliente.vue";
+import Client from "./components/Client.vue";
 export default {
   name: "App",
   data() {
     return {
-      nomedovictor: "j. victor o. lima",
-      clientevictor: {
-        nome: "victor lima",
-        email: "victor@gmail.com",
-        idade: 99,
+      admin: {
+        name: "lucas",
+        sex: "M",
+        email: "lucas@gmail.com",
+        age: 99,
+        showAge: true,
       },
+      clients: [
+        {
+          id: 22,
+          name: "lucas",
+          sex: "F",
+          email: "lucas@silva.com",
+          age: 60,
+          showAge: false,
+        },
+        {
+          id: 38,
+          name: "andrew gustavo",
+          sex: "F",
+          email: "gustavoandrew@gmail.com",
+          age: 35,
+          showAge: true,
+        },
+        {
+          id: 79,
+          name: "matheus barbosa",
+          sex: "M",
+          email: "matheus@barbosa.com",
+          age: 99,
+          showAge: true,
+        },
+        {
+          id: 38,
+          name: "vanderlei",
+          sex: "M",
+          email: "vanderlei@gmail.com",
+          age: 35,
+          showAge: false,
+        },
+      ],
     };
   },
   components: {
-    Cliente,
+    Client,
   },
 };
 </script>
---
+++ vue/guiaclientes/src/components/Client.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="{ client: !ispremium, 'client-premium': ispremium }">
+    <h4>name: {{ client.name }}</h4>
+    <hr />
+    <p>email: {{ client.email }}</p>
+    <span>Digite sua observação:</span>
+    <br />
+    <input type="text" v-model="message" placeholder="Digite aqui" />
+    <p>
+      <input type="checkbox" id="checkbox" v-model="checked" /> Mostar/Ocultar
+      idade
+    </p>
+
+    <p v-if="checked === true">idade: {{ client.age }}.</p>
+
+    <p v-else-if="client.sex === 'F' && client.age >= 60">
+      A senhora preferiu ocultar a idade
+    </p>
+    <p v-else-if="client.sex === 'F' && client.age < 10">
+      A criança preferiu ocultar a idade
+    </p>
+    <p v-else-if="client.sex === 'F'">A usuaria não se revelou.</p>
+    <p v-else-if="client.sex === 'M' && client.age >= 60">
+      O senhor preferiu ocultar a idade
+    </p>
+    <p v-else-if="client.sex === 'M' && client.age < 10">
+      O criansso preferiu ocultar a idade
+    </p>
+    <p v-else-if="client.sex === 'M'">O usuario não se revelou.</p>
+    <p v-else-if="client.age >= 60">
+      A coisa aparentemente arcaico ou/a acaica não se revelou.
+    </p>
+    <p v-else-if="client.age < 10">A coisa não se revelou.</p>
+
+    <p v-else>Erro de dados informe o adm bizonho.</p>
+    <p v-if="message === 'Nao sou programador'">User</p>
+    <p v-if="message === 'É programador'">Bem vindo mestre.</p>
+    <p v-else>Observação sobre o individuo: {{ message }}.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ispremium: true,
+    };
+  },
+  props: {
+    client: Object,
+    checked: Boolean,
+    message: String,
+  },
+};
+</script>
+
+<style scoped>
+.Cliente {
+  background-color: black;
+  max-width: 600px;
+  height: 210px;
+  padding: 1%;
+  margin-top: 1%;
+}
+
+.client-premium {
+  background: black;
+  color: yellowgreen;
+  max-width: 600px;
+  height: 280px;
+  padding: 1%;
+  margin-top: 2%;
+}
+</style>
